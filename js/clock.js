3c8d178da3e2719ae7a855b8b1951b04b62a248c
Mesut Çifci <mesutcifci97@gmail.com>
2021-03-28 18:24:17 +0300

    fix typo getUserName created setInterval changed to setTimeout

--- js/clock.js
+++ js/clock.js
@@ -2,27 +2,32 @@ let name = document.getElementById("myName");
 let text2 = document.getElementById("text2");
 let dateField = document.getElementById("date-field")
 
-name.textContent = prompt("Lütfen Adınızı Giriniz", "Guest");
+function getUserName() {
+    name.textContent = prompt("Please enter your name!", "Guest");
+}
+
+
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', timezone: 'UTC' };
 
 function showTime() {
 
-    setInterval(function () {
-        let date = new Date().toLocaleTimeString('en-EN', options);
-        date = date.split(" ");
-    
-        let time = date[4].trim();
-        let timeLength = time.length;
-        Number(time[0]) < 10 ? time = time.padStart(timeLength + 1, "0") : false;
-    
-        date.splice(4, 1, time);
-        date = date.join(" ");
-    
-        dateField.textContent = date;
-    
-    }, 1000)
-    
+
+    let date = new Date().toLocaleTimeString('en-EN', options);
+    date = date.split(" ");
+
+    let time = date[4].trim();
+    let timeLength = time.length;
+    Number(time[0]) < 10 ? time = time.padStart(timeLength + 1, "0") : false;
+
+    date.splice(4, 1, time);
+    date = date.join(" ");
+
+    dateField.textContent = date;
+
+    setTimeout(showTime, 1000);
+
 }
 
+getUserName();
 showTime();
 
